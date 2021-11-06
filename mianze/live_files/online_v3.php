1f268cbfa8c1cea66a4e7e54c4216bd3cfb598f1
aboshi8 <1367863704@qq.com>
2021-11-06 19:11:57 +0800

    Site updated: 2021-11-06 19:11:06

---
+++ mianze/live_files/online_v3.php
@@ -0,0 +1,6 @@
+var node=document.getElementById('cnzz_stat_icon_'+1280306980);
+if(node){
+    node.innerHTML="<a href=https://www.cnzz.com/stat/website.php?web_id=1280306980 target=_blank>站长统计</a><a href=https://www.cnzz.com/stat/website.php?web_id=1280306980 target=_blank> |  今日IP[99] | 今日PV[308] | 昨日IP[97] |  昨日PV[331] | <a href=https://www.cnzz.com/stat/website.php?web_id=1280306980&method=online target=_blank>当前在线[1]</a>";
+}else{
+    document.write("<a href=https://www.cnzz.com/stat/website.php?web_id=1280306980 target=_blank>站长统计</a><a href=https://www.cnzz.com/stat/website.php?web_id=1280306980 target=_blank> |  今日IP[99] | 今日PV[308] | 昨日IP[97] |  昨日PV[331] | <a href=https://www.cnzz.com/stat/website.php?web_id=1280306980&method=online target=_blank>当前在线[1]</a>");
+}
